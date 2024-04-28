64212d1f4539b6c1c7a0ff50af03728454c0cddd
LEYuJeong <nania000822@naver.com>
2024-04-28 20:57:51 +0900

    GunController

---
+++ Assets/02.Scripts/GunController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
