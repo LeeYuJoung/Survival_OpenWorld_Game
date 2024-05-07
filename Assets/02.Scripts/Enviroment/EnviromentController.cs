e54f6fc5b671736bd1dafa946343ca51eb52f2d4
LeeYuJoung <105534978+LeeYuJoung@users.noreply.github.com>
2024-05-07 15:51:53 +0900

    EnviromentController

--- Assets/02.Scripts/Enviroment/EnviromentController.cs
+++ Assets/02.Scripts/Enviroment/EnviromentController.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class EnviromentController : MonoBehaviour
 {
+    public enum EnviromentType
+    {
+        Tree,
+        Rock
+    }
+    public EnviromentType currentType;
+
+    public GameObject[] treeItems;
+    private Vector3 originPos;
+
     [SerializeField]
     private float hp;
+    private float currentHP;
+
+    [SerializeField]
+    private float shakeTime;
+    private float currentShakeTime;
 
     void Start()
     {
-        
+        originPos = transform.position;
+        currentHP = hp;
     }
 
     void Update()
@@ -17,9 +33,47 @@ public class EnviromentController : MonoBehaviour
         
     }
 
+    // 채굴
+    public void Mining(float _damage)
+    {
+        SpawnItem();
+        hp -= _damage;
+
+        if(hp <= 0)
+        {
+            Destruction();
+        }
+    }
+
+    // 아이템 생성
+    public void SpawnItem()
+    {
+        if(currentType == EnviromentType.Tree)
+        {
+            GameObject _item = Instantiate(treeItems[Random.Range(0, treeItems.Length)]);
+            _item.transform.position = new Vector3(Random.Range(-0.1f, 0.1f), 0, 0);
+        }
+    }
+
     // 파괴
     public void Destruction()
     {
+        Debug.Log("::: 제거 완료 :::");
+        transform.gameObject.SetActive(false);
+    }
 
+    // 채굴 작업 시 흔들림 효과
+    public IEnumerator EnviromentShake()
+    {
+        currentShakeTime = 0;
+
+        while (currentShakeTime < shakeTime)
+        {
+            transform.position += new Vector3(Random.Range(-0.0025f, 0.0025f), 0, 0);
+            yield return new WaitForSeconds(0.01f);
+            currentShakeTime += Time.deltaTime;
+        }
+
+        transform.position = originPos;
     }
 }
--- Assets/02.Scripts/Player/PlayerController.cs
+++ Assets/02.Scripts/Player/PlayerController.cs
@@ -58,6 +58,7 @@ public class PlayerController : MonoBehaviour
         else if(Input.GetKeyUp(KeyCode.Keypad1) || Input.GetKeyUp(KeyCode.Alpha1))
         {
             isAx = false;
+            currentWorkDelayTime = 0;
             statusController.isSPUse = false;
         }
         else if(statusController.GetCurrentSP() <= 0)
@@ -80,12 +81,12 @@ public class PlayerController : MonoBehaviour
             {
                 Debug.Log("³ª¹« Á¦°Å Áß....");
                 currentWorkDelayTime += Time.deltaTime;
+                StartCoroutine(hit.collider.GetComponent<EnviromentController>().EnviromentShake());
 
-                if(currentWorkDelayTime >= workDelayTime)
+                if (currentWorkDelayTime >= workDelayTime)
                 {
                     currentWorkDelayTime = 0;
-                    hit.collider.GetComponent<EnviromentController>();
-                    Debug.Log("::: ³ª¹µ°¡Áö È¹µæ :::");
+                    hit.collider.GetComponent<EnviromentController>().Mining(20.0f);
                 }
             }
         }
@@ -96,15 +97,17 @@ public class PlayerController : MonoBehaviour
         if (isAx)
             return;
 
-        if(Input.GetKey(KeyCode.Keypad0) || Input.GetKey(KeyCode.Alpha0))
+        if(Input.GetKey(KeyCode.Keypad2) || Input.GetKey(KeyCode.Alpha2))
         {
             isHammer = true;
             playerMovement.isWalk = false;
+            statusController.DecreaseSP(0.1f);
             InteractionHammer();
         }
-        else if(Input.GetKeyUp(KeyCode.Keypad0) || Input.GetKeyUp(KeyCode.Alpha0))
+        else if(Input.GetKeyUp(KeyCode.Keypad2) || Input.GetKeyUp(KeyCode.Alpha2))
         {
             isHammer = false;
+            currentWorkDelayTime = 0;
             statusController.isSPUse = false;
         }
         else if (statusController.GetCurrentSP() <= 0)
@@ -119,7 +122,6 @@ public class PlayerController : MonoBehaviour
     {
         int layerMask = ~((1 << LayerMask.NameToLayer("Player")) + (1 << LayerMask.NameToLayer("Grid")));
         hit = Physics2D.Raycast(transform.position, playerMovement.playerDirection, 1.0f, layerMask);
-        Debug.DrawRay(transform.position, playerMovement.playerDirection, Color.red, 1.0f);
 
         if (hit)
         {
@@ -127,12 +129,12 @@ public class PlayerController : MonoBehaviour
             {
                 Debug.Log("µ¹ Á¦°Å Áß....");
                 currentWorkDelayTime += Time.deltaTime;
+                StartCoroutine(hit.collider.GetComponent<EnviromentController>().EnviromentShake());
 
                 if (currentWorkDelayTime >= workDelayTime)
                 {
                     currentWorkDelayTime = 0;
-                    hit.collider.GetComponent<EnviromentController>();
-                    Debug.Log("::: µ¹¸æÀÌ È¹µæ :::");
+                    hit.collider.GetComponent<EnviromentController>().Mining(20.0f);
                 }
             }
         }
