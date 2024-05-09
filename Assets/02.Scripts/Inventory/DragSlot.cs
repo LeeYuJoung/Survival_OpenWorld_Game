cdea5ef317915a7e381398fb79013fa46d5c14b0
LeeYuJoung <105534978+LeeYuJoung@users.noreply.github.com>
2024-05-09 17:27:16 +0900

    Drag Slot

---
+++ Assets/02.Scripts/Inventory/DragSlot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragSlot : MonoBehaviour
+{
+    public static DragSlot instance;
+    public Slot dragSlot;
+
+    [SerializeField]
+    private Image itemImage;
+
+    void Start()
+    {
+        instance = this;
+        itemImage = GetComponent<Image>();
+    }
+
+    public void DragSetImage(Image _itemImage)
+    {
+        itemImage.sprite = _itemImage.sprite;
+        SetColor(1);
+    }
+
+    public void SetColor(float _alpha)
+    {
+        Color color = itemImage.color;
+        color.a = _alpha;
+        itemImage.color = color;
+    }
+}
--- Assets/02.Scripts/Inventory/Slot.cs
+++ Assets/02.Scripts/Inventory/Slot.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Slot : MonoBehaviour, IPointerClickHandler
+public class Slot : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
     // 획득한 아이템
     public Item item;
@@ -64,5 +65,86 @@ public class Slot : MonoBehaviour, IPointerClickHandler
 
         text_Count.text = "0";
         text_Count.gameObject.SetActive(false);
+    }
+
+    // IPointerClickHandler 인터페이스 상속 시 마우스 클릭 이벤트 받기 가능
+    // PointerEventData는 마우스 혹은 터치 입력 이벤트에 관한 정보 저장 (이벤트가 들어온 버튼, 클릭 수, 마우스 위치 등)
+    // 오브젝트에 마우스 클릭 이벤트 발생 시 호출
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Right)
+        {
+            if(item != null)
+            {
+                if(item.itemType == Item.ItemType.Equipment)
+                {
+                    // 무기 장착
+                }
+                else
+                {
+                    // 아이템 소비
+                    Debug.Log(item.itemName + " 을 사용했습니다....");
+                    SetSlotCount(-1);
+                }
+            }
+        }
+    }
+
+    // 드래그가 되는 대상에서 호출
+    // 마우스 드래그가 시작 됐을 때 발생하는 이벤트
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if(item != null)
+        {
+            DragSlot.instance.dragSlot = this;
+            DragSlot.instance.DragSetImage(itemImage);
+            DragSlot.instance.transform.position = eventData.position;
+        }
+    }
+
+    // 드래그가 되는 대상에서 계속 호출
+    // 마우스 드래그 중일 때 계속 발생하는 이벤트
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(item != null)
+        {
+            DragSlot.instance.transform.position = eventData.position;
+        }
+    }
+
+    // 나 자신을 드래그 하는 것을 끝냈을 때 드래그 대상 오브젝트에서 호출
+    // 마우스 드래그가 끝났을 때 발생하는 이벤트
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        DragSlot.instance.SetColor(0);
+        DragSlot.instance.dragSlot = null;
+    }
+
+    // 내 자신한테 드롭 된 무언가가 있을 때 호출
+    // 해당 슬롯에 무언가의 마우스 드롭 됐을 때 발생하는 이벤트
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(DragSlot.instance.dragSlot != null)
+        {
+            Debug.Log("Drop Item");
+            ChangeSlot();
+        }
+    }
+    
+    public void ChangeSlot()
+    {
+        Item _tempItem = item;
+        int _tempItemCount = itemCount;
+
+        AddItem(DragSlot.instance.dragSlot.item, DragSlot.instance.dragSlot.itemCount);
+
+        if(_tempItem != null)
+        {
+            DragSlot.instance.dragSlot.AddItem(_tempItem, _tempItemCount);
+        }
+        else
+        {
+            DragSlot.instance.dragSlot.ClearSlot();
+        }
     }
 }
