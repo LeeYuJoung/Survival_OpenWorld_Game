ef29ef23ea3e92f37c1d8fb92b5edc8ab4c213dc
LeeYuJoung <105534978+LeeYuJoung@users.noreply.github.com>
2024-05-14 17:29:54 +0900

    ItemEffectDatabase

--- Assets/02.Scripts/Inventory/InputNumber.cs
+++ Assets/02.Scripts/Inventory/InputNumber.cs
@@ -37,17 +37,47 @@ public class InputNumber : MonoBehaviour
 
     public void Call()
     {
-
+        go_Base.SetActive(true);
+        activated = true;
+        if_text.text = "";
+        text_Preview.text = DragSlot.instance.dragSlot.itemCount.ToString();
     }
 
     public void Cancel()
     {
-
+        activated = false;
+        DragSlot.instance.SetColor(0);
+        go_Base.SetActive(false);
+        DragSlot.instance.dragSlot = null;
     }
 
     public void OK()
     {
+        DragSlot.instance.SetColor(0);
+        int num = 0;
 
+        if(text_Input.text != "")
+        {
+            if (CheckNumber(text_Input.text))
+            {
+                num = int.Parse(text_Input.text);
+
+                if(num > DragSlot.instance.dragSlot.itemCount)
+                {
+                    num = DragSlot.instance.dragSlot.itemCount;
+                }
+                else
+                {
+                    num = 1;
+                }
+            }
+            else
+            {
+                num = int.Parse(text_Preview.text);
+            }
+        }
+
+        StartCoroutine(DropItemCoroutine(num));
     }
 
     IEnumerator DropItemCoroutine(int _num)
@@ -57,7 +87,15 @@ public class InputNumber : MonoBehaviour
 
     public bool CheckNumber(string _string)
     {
+        char[] _tempCharArray = _string.ToCharArray();
         bool isNumber = true;
+
+        for(int i = 0; i < _tempCharArray.Length; i++)
+        {
+            if (_tempCharArray[i] >= 48 && _tempCharArray[i] <= 57)
+                continue;
+            isNumber = false;
+        }
 
         return isNumber;
     }
---
+++ Assets/02.Scripts/Item/ItemEffectDatabase.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// MonoBehaviour를 상속 받는 클래스가 아닌 일반 C# 클래스임에도 불구하고 유니티 인스펙터에 띄워 값 할당 가능하게 함
+[System.Serializable]
+public class ItemEffect
+{
+    public string itemName;  // 아이템 이름
+    [Tooltip("HP, MP, SP, HUNGRY, THIRSTY, SATISFY 만 가능")]
+    public string[] part;    // 효과, 어느 부분을 회복하거나 혹은 깎을 것인지
+    public int[] num;        // 수치, 포션 하나당 미치는 수치
+}
+
+// 여러 포션 아이템을 한 번에 관리하기 위한 스크립트
+public class ItemEffectDatabase : MonoBehaviour
+{
+    [SerializeField]
+    private ItemEffect[] itemEffects;
+
+    private const string HP = "HP", MP = "MP", SP = "SP", HUNGRY = "HUNGRY", THIRSTY = "THIRSTY", SATISFY = "SATISFY";
+
+    [SerializeField]
+    private StatusController statusController;
+    
+    public void UseItem(Item _item)
+    {
+        if(_item.itemType == Item.ItemType.Equipment)
+        {
+
+        }
+        if(_item.itemType == Item.ItemType.Used)
+        {
+            for(int i = 0; i < itemEffects.Length; i++)
+            {
+                if (itemEffects[i].itemName == _item.itemName)
+                {
+                    for(int j = 0; j < itemEffects[i].part.Length; j++)
+                    {
+                        switch (itemEffects[i].part[j])
+                        {
+                            case HP:
+                                statusController.IncreaseHP(itemEffects[i].num[j]);
+                                break;
+                            case MP:
+                                statusController.IncreaseMP(itemEffects[i].num[j]);
+                                break;
+                            case SP:
+                                
+                                break;
+                            case HUNGRY:
+                                statusController.IncreaseHungry(itemEffects[i].num[j]);
+                                break;
+                            case THIRSTY:
+                                statusController.IncreaseThirsty(itemEffects[i].num[j]);
+                                break;
+                            case SATISFY:
+                                break;
+                            default:
+                                Debug.Log("잘못된 Status 부위....");
+                                break;
+                        }
+
+                        Debug.Log(_item.itemName + "을 사용....");
+                    }
+                    return;
+                }
+            }
+            Debug.Log("!!! ItemEffectDatabase에 일치하는 itemName이 없습니다 !!!");
+        }
+    }
+}
